e0b12c67d719d0edb75c7efc3bb04d24529b45fc
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-06-24 20:33:15 -0300

    inserir

--- projetosistema/index.js
+++ projetosistema/index.js
@@ -18,10 +18,12 @@ const fakedata = [
         telefone: '5555-4321'
     }
 ];
-
+/*Configura a engine (motor) do express para utilizar o handlebars */
+app.use(bodyparser.urlencoded({extended: false}));
 app.set('view engine','handlebars');
 app.engine('handlebars', engine());
 
+/*disponibilizando acesso para as bibliotecas estaticas do bootstrap e jquery */
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
@@ -32,12 +34,29 @@ app.get('/', function(req,res){
     res.render('index');
 });
 
+app.get('/clientes/novo', function(req,res){
+    res.render('formcliente');
+});
+
+app.post('/clientes/save', function(req,res){
+    let novocliente = {
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        sexo: req.body.sexo,
+        telefone: req.body.telefone,
+        id: 0
+    };
+    fakedata.push(novocliente);
+    res.redirect("/clientes");
+});
+
+
 app.get('/clientes', function(req,res){
     //res.send("<h1>eu nao acredito</h1>");
     res.render('clientes', {listaclientes: fakedata});
 });
 
-
+/*inicialização da aplicação NodeJS + Express */
 app.listen(3000, () =>{
-    console.log('Server online');
+    console.log('Server online - http://localhost:3000/');
 });
